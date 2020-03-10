542fb5098a3f8124f4c9dda9daa9c47ac9633901
Ali Shalabi <ashalabi13@gmail.com>
2020-03-09 22:33:54 -0700

    Input converted to uppercase

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 class SongTitleFinder:
     def __init__(self, input, corpus):
         self.input = input.upper()
-        self.corpus = corpus.upper()
+        self.corpus = set()
+        for key in corpus:
+            self.corpus.add(key.upper())
         # Converted input will come from convert_input()
         self.converted_input = None
 
@@ -20,5 +22,9 @@ class SongTitleFinder:
 
 
 sample_input1 = "Hey ma, I though that I told you I like the way you move"
-sample_song_dict1 = {"HEY ME"}
-test1 = SongTitleFinder()
+sample_song_dict1 = {"HEY MA", "I Like the way YOU move",
+                     "happy days", "Is a good show"}
+test1 = SongTitleFinder(sample_input1, sample_song_dict1)
+# Seeing if sample input and sample dict are properly converted
+print(f"Converted sample input: {test1.input}")
+print(f"Converted sample dict: {test1.corpus}")
