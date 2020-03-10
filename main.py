21c5f9c8e7dc517ca55f5c5789c5527fdc3ebd95
Ali Shalabi <ashalabi13@gmail.com>
2020-03-09 22:52:12 -0700

    First major bug, going to sleep

--- main.py
+++ main.py
@@ -15,10 +15,16 @@ class SongTitleFinder:
         if current_combo == None:
             current_combo = []
         # Exit condition: no more items to iterate
+        if current_index == len(self.input) - 1:
+            return all_combinations
         # Iterate through each first word in remaining array
+        for index in range(current_index, len(self.input)):
             # Add word to current combo
+            current_combo.append(self.input[index])
             # Add combo to all combos
+            all_combinations.append(current_combo)
             # Recursively iterate through each remaining word in remaining array
+            return self.recursive_search(index, current_combo, all_combinations)
 
 
 sample_input1 = "Hey ma I though that I told you I like the way you move"
@@ -28,3 +34,5 @@ test1 = SongTitleFinder(sample_input1, sample_song_dict1)
 # Seeing if sample input and sample dict are properly converted
 print(f"Converted sample input: {test1.input}")
 print(f"Converted sample dict: {test1.corpus}")
+
+print(test1.recursive_search())
